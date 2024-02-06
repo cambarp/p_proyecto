86faa35b578bc02c6dda9ce9712a63eb3b481de4
nilcambar@gmail.com <nilcambar@gmail.com>
2024-02-06 12:12:37 +0100

    actualizando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { Routes,RouterModule} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
@@ -9,17 +7,15 @@ import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 export const routes: Routes = [
 
-    { path: '', component: AppComponent },
     { path: 'header', component: HeaderComponent },
     { path: 'cuerpo', component: CuerpoComponent},
     { path: 'quienes_somos', component: QuienesSomosComponent}
 ];
 @NgModule({
-    
     imports: [
-      
       RouterModule.forRoot(routes)
     ],
-   
+    exports :[RouterModule]
   })
+
   export class AppModule { }
