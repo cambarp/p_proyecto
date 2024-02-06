734b1d1b3f64aafa2caa8de6dff78e77999803a7
nilcambar@gmail.com <nilcambar@gmail.com>
2024-02-06 11:35:08 +0100

    creando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,CuerpoComponent,QuienesSomosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path: '', component: AppComponent },
+    { path: 'header', component: HeaderComponent },
+    { path: 'cuerpo', component: CuerpoComponent},
+    { path: 'quienes_somos', component: QuienesSomosComponent}
+];
+@NgModule({
+    
+    imports: [
+      
+      RouterModule.forRoot(routes)
+    ],
+   
+  })
+  export class AppModule { }
